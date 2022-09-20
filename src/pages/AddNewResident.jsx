37a9cf9e2882d6c0d671b8e9ac45dff8ccb833f5
pieceoftrash8903 <56954478+pieceoftrash8903@users.noreply.github.com>
2022-09-20 14:35:12 +1000

    Add resident and staff pages just need linking to backend

--- src/pages/AddNewResident.jsx
+++ src/pages/AddNewResident.jsx
@@ -1,48 +1,95 @@
-import React , { useState } from 'react';
-import DatePicker from 'react-datepicker';
+import React from 'react';
 import "../App.css"
-import "react-datepicker/dist/react-datepicker.css";
-import Moment from 'moment';
+import Popup from 'reactjs-popup';
 
-function AddNewResident() {
-    const [name, setName] = useState("");
-    const [apartment, setApartment] = useState("");
-    const [startDate = Moment().format('DD-MM-YYYY'), setStartDate] = useState(new Date());
+export default class AddNewResident extends React.Component {
+  constructor(props) {
+    super(props);
+    // JSON format for a resident
+    this.state = {resident: {
+        firstName: props.firstName,
+        lastName: props.lastName,
+        apartmentNo: props.apartmentNo,
+        dob: props.dob
+      }
+    }
+  }
+  
+  // handle changes of variables
+  handleFirstNameChanged(event) {
+    var resident = this.state.resident;
+    resident.firstName = event.target.value;
+    this.setState({ resident: resident });
+  }
+
+  handleLastNameChanged(event) {
+    var resident = this.state.resident;
+    resident.lastName = event.target.value;
+    this.setState({ resident: resident });
+  }
+
+  handleApartmentNoChanged(event) {
+    var resident = this.state.resident;
+    resident.apartmentNo = event.target.value;
+    this.setState({ resident: resident });
+  }
+
+  handleDOBChanged(event){
+    var resident = this.state.resident;
+    resident.dob = event.target.value;
+    this.setState({resident: resident});
+  }
+
+  // Force console to log state of resident
+  handleButtonClicked() {
+    console.log(this.state.resident);
+  }
+
+  render() {
     return (
-        <p className='Font'>
+      <div className='Font'>
         <table className='Font'>
-            <tr>
-                <th>Name</th>
-                <td>
-                    <form>
-                        <input className='Input' type="text" value={name} 
-                            onChange={(e) => setName(e.target.value)}/>
-                    </form>
-                </td>
-            </tr>
-            <tr>
-                <th>Apartment Number</th>
-                <td>
-                    <form>
-                        <input className='Input' type="number" 
-                            onChange={(e) => setApartment(e.target.value)}/>
-                    </form>
-                </td>
-            </tr>
-            <tr>
-                <th>Date of Birth</th>
-                <td>
-                    <DatePicker selected={startDate} 
-                    onChange={(date) => setStartDate(date)}
-                    dateFormat='dd/MM/yyyy'
-                    className='Input'/>
-                </td>
-            </tr>
+          <tr>
+            <th>First Name</th>
+              <td>
+                <input type="text" className = 'Input '
+                value={this.state.resident.firstName} 
+                onChange={this.handleFirstNameChanged.bind(this)}/>
+              </td>
+          </tr>
+          <tr>
+            <th>Last Name</th>
+              <td>
+                <input type="text" className = 'Input '
+                value={this.state.resident.lastName} 
+                onChange={this.handleLastNameChanged.bind(this)}/>
+              </td>
+          </tr>
+          <tr>
+            <th>Apartment Number</th>
+              <td>
+                <form>
+                    <input className='Input' type="number" 
+                    value={this.state.resident.apartmentNo}
+                    onChange={this.handleApartmentNoChanged.bind(this)}/>
+                </form>
+              </td>
+          </tr>
+          <tr>
+            <th>Date of Birth</th>
+              <td>
+              <input className='Input' type="date" 
+                    value={this.state.resident.dob}
+                    onChange={this.handleDOBChanged.bind(this)}/>
+              </td>
+          </tr>
         </table>
-        <br></br>
-        {name} {apartment} {startDate.toLocaleString()}
-        </p>
+        <Popup
+          trigger={<button onClick={this.handleButtonClicked.bind(this)}>
+            Add Resident</button>}>
+                <div> Resident Added!</div>
+            </Popup>
+      </div>
     );
+  }
 }
-
-export default AddNewResident;
--- src/pages/AddNewStaff.jsx
+++ src/pages/AddNewStaff.jsx
@@ -1,52 +1,83 @@
-import React, {useState} from 'react';
-import DatePicker, { registerLocale } from 'react-datepicker';
+import React from 'react';
 import "../App.css"
-import "react-datepicker/dist/react-datepicker.css";
-import Moment from 'moment';
+import Popup from 'reactjs-popup';
 
-function AddNewStaff() {
-    const [name, setName] = useState("");
-    const [role, setMyRole] = useState("Employee");
-
-    const handleChange = (event) => {setMyRole(event.target.value)}
-    const [startDate = Moment().format('DD-MM-YYYY'), setStartDate] = useState(new Date());
-    return (
-        <p className='Font'>
-        <table className='Font'>
+export default class AddNewStaff extends React.Component {
+    constructor(props) {
+      super(props);
+      // JSON format for a staff member
+      this.state = {staff: {
+          firstName: props.firstName,
+          lastName: props.lastName,
+          dob: props.dob
+        }
+      }
+    }
+    // handle changes of variables
+    handleFirstNameChanged(event) {
+      var staff = this.state.staff;
+      staff.firstName = event.target.value;
+      this.setState({ staff: staff });
+    }
+  
+    handleLastNameChanged(event) {
+      var staff = this.state.staff;
+      staff.lastName = event.target.value;
+      this.setState({ staff: staff });
+    }
+  
+    handleApartmentNoChanged(event) {
+      var staff = this.state.staff;
+      staff.apartmentNo = event.target.value;
+      this.setState({ staff: staff });
+    }
+  
+    handleDOBChanged(event){
+      var staff = this.state.staff;
+      staff.dob = event.target.value;
+      this.setState({staff: staff});
+    }
+  
+    // Force console to log state of staff
+    handleButtonClicked() {
+      console.log(this.state.staff);
+    }
+  
+    render() {
+      return (
+        <div className='Font'>
+          <table className='Font'>
             <tr>
-                <th>Name</th>
+              <th>First Name</th>
                 <td>
-                    <form>
-                        <input className='Input' type="text" value={name} 
-                            onChange={(e) => setName(e.target.value)}/>
-                    </form>
+                  <input type="text" className = 'Input '
+                  value={this.state.staff.firstName} 
+                  onChange={this.handleFirstNameChanged.bind(this)}/>
                 </td>
             </tr>
             <tr>
-                <th>Role</th>
+              <th>Last Name</th>
                 <td>
-                    <form>
-                        <select className = 'Input' selected={role} value={role} onChange={handleChange}>
-                            <option value="Employee">Employee</option>
-                            <option value="Manager">Manager</option>
-                        </select>
-                    </form>
+                  <input type="text" className = 'Input '
+                  value={this.state.staff.lastName} 
+                  onChange={this.handleLastNameChanged.bind(this)}/>
                 </td>
             </tr>
             <tr>
-                <th>Date of Birth</th>
+              <th>Date of Birth</th>
                 <td>
-                    <DatePicker selected={startDate} 
-                    onChange={(date) => setStartDate(date)}
-                    dateFormat='dd/MM/yyyy'
-                    className='Input'/>
+                <input className='Input' type="date" 
+                      value={this.state.staff.dob}
+                      onChange={this.handleDOBChanged.bind(this)}/>
                 </td>
             </tr>
-        </table>
-        <br></br>
-        {name} {role.toString()} {startDate.toLocaleString()}
-        </p>
-    );
-}
-
-export default AddNewStaff;
+          </table>
+          <Popup
+          trigger={<button onClick={this.handleButtonClicked.bind(this)}>
+            Add Staff Member</button>}>
+                <div> Staff member Added! </div>
+            </Popup>
+        </div>
+      );
+    }
+  }

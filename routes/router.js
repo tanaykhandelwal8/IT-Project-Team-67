19bd58323ee534c1f062772c22d63c0679be2249
Panos Menidis <panosmenidis@Panoss-MacBook-Pro.local>
2022-09-21 17:04:48 +1000

    trying to get this working

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const path = require('path')
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
 

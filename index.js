b7ba9a5b2fde39d0263124a99beae41a5cb3ae5e
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-09-27 10:07:07 +1000

    updating with my old branch

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.static(__dirname))
 
+// Connect to DB
+mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: 'residencly'
+});
+
+// connect-mongo
+const mongooseClient = mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost', { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    dbName: 'residencly' 
+}).then((m) => m.connection.getClient())
+
+// Exit on error
+const db = mongoose.connection.on('error', err => {
+    console.error(err);
+    process.exit(1);
+});
+// Log to console once the database is open
+db.once('open', async () => {
+    console.log(`Mongo connection started on ${db.host}:${db.port}`);
+});
+
 // TODO: add handlebars handling code, can do after adding handlebars helpers
 
 var indexRouter = require('./routes/router')
---
+++ models/image.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+  
+var imageSchema = new mongoose.Schema({
+    name: String,
+    desc: String,
+    img:
+    {
+        data: Buffer,
+        contentType: String
+    }
+});
+  
+//Image is a model which has a schema imageSchema
+  
+module.exports = new mongoose.model('Image', imageSchema)
+module.exports = Image
+
+//https://www.geeksforgeeks.org/upload-and-retrieve-image-on-mongodb-using-mongoose/

125dc5048270d37305e2ef2bd75d8bec831e6cd6
srimanokaran <59358662+srimanokaran@users.noreply.github.com>
2022-10-03 01:30:10 +1100

    MusicPreference.jsx

---
+++ src/pages/MusicPreference.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+const MusicPreference = () => {
+    return(
+        <><img src="../gui/assets/Residencely-1.png" width="100" height="100" alt="Residencely">
+            <a href="IT_Project_Team_67.pdf"
+                target="_blank" style="float:right;">Log out</a>
+            <br>
+                <h1>Music Preferences</h1>
+                <br>
+                    <br>
+                        <div class="row">
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song1">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song2">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song3">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song4">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song1">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song2">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                            <div class="column">
+                                <img src="../gui/assets/trebleclef.jpeg" alt="Song3">
+                                    <p>Song Artist</p>
+                                    <p>Genre</p>
+                                </></div>
+                        </div>
+                    </br>
+                </br>
+            </br>
+        </br><p style="float: right;">Signed in as Xxxx Xxxxxx</p></>
+    )
+}
+
+export default Layout;

c32699fec7b0498815b85745fb84dbcd644b21a9
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-06 20:06:50 +1100

    images to models

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,32 @@ const PORT = process.env.PORT || 6000
 require('./models/index')
 app.use(express.static(__dirname))
 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+  
+var fs = require('fs');
+var path = require('path');
+require('dotenv/config');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+  
+// Set EJS as templating engine 
+app.set("view engine", "ejs");
+
+var multer = require('multer');
+  
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+  
+var upload = multer({ storage: storage });
+
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })

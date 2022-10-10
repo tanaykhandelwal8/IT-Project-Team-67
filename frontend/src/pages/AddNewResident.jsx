3bf014579cdbc1cfa8ed483fc34b6458f34aa97c
pieceoftrash8903 <56954478+pieceoftrash8903@users.noreply.github.com>
2022-10-10 16:05:43 +1100

    changed for tanay use

--- frontend/src/pages/AddNewResident.jsx
+++ frontend/src/pages/AddNewResident.jsx
@@ -1,96 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import "../App.css"
 import Popup from 'reactjs-popup';
 
-export default class AddNewResident extends React.Component {
-  constructor(props) {
-    super(props);
-    // JSON format for a resident
-    this.state = {resident: {
-        firstName: props.firstName,
-        lastName: props.lastName,
-        address: props.address,
-        dob: props.dob
-      }
-    }
-  }
 
-  // handle changes of variables
-  handleFirstNameChanged(event) {
-    var resident = this.state.resident;
-    resident.firstName = event.target.value;
-    this.setState({ resident: resident });
-  }
+function AddNewResident()  {
+  // temporary constants
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [location, setLocation] = useState("")
+  const [dob, setDOB] = useState("")
 
-  handleLastNameChanged(event) {
-    var resident = this.state.resident;
-    resident.lastName = event.target.value;
-    this.setState({ resident: resident });
-  }
-
-  handleAddressChanged(event) {
-    var resident = this.state.resident;
-    resident.address = event.target.value;
-    this.setState({ resident: resident });
-  }
-
-  handleDOBChanged(event){
-    var resident = this.state.resident;
-    resident.dob = event.target.value;
-    this.setState({resident: resident});
-  }
-
-  // Force console to log state of resident
-  handleButtonClicked() {
-    console.log(this.state.resident);
-  }
-
-  render() {
-    return (
-        <div className='Font'>
-          <table className='Font'>
-            <tr>
-              <th>First Name</th>
+  return (
+      <div className='Font'>
+        <table className='Font'>
+          <tr>
+            <th>First Name</th>
               <td>
                 <input type="text" className = 'Input '
-                       value={this.state.resident.firstName}
-                       onChange={this.handleFirstNameChanged.bind(this)}/>
+                value={firstName} 
+                onChange={e => setFirstName(e.target.value)}/>
               </td>
             </tr>
             <tr>
               <th>Last Name</th>
               <td>
                 <input type="text" className = 'Input '
-                       value={this.state.resident.lastName}
-                       onChange={this.handleLastNameChanged.bind(this)}/>
+                value={lastName} 
+                onChange={e => setLastName(e.target.value)}/>
               </td>
-            </tr>
-            <tr>
-              <th>Address</th>
+          </tr>
+          <tr>
+            <th>Location</th>
               <td>
                 <form>
-                  <input className='Input' type="text"
-                         value={this.state.resident.address}
-                         onChange={this.handleAddressChanged.bind(this)}/>
+                    <input className='Input' type="text" 
+                    value={location}
+                    onChange={e => setLocation(e.target.value)}/>
                 </form>
               </td>
             </tr>
             <tr>
               <th>Date of Birth</th>
               <td>
-                <input className='Input' type="date"
-                       value={this.state.resident.dob}
-                       onChange={this.handleDOBChanged.bind(this)}/>
+              <input className='Input' type="date" 
+                    value={dob}
+                    onChange={e => setDOB(e.target.value)}/>
               </td>
-            </tr>
-          </table>
-          {/* WILL BE CHANGED TO PROFILE OF NEW RESIDENT */}
-          <Popup
-              trigger={<button onClick={this.handleButtonClicked.bind(this)}>
-                Add Resident</button>}>
-            <div> Resident Added!</div>
-          </Popup>
-        </div>
+          </tr>
+        </table>
+        {/* WILL BE CHANGED TO PROFILE OF NEW RESIDENT */}
+        <button onClick={console.log(firstName, lastName, location, dob)}>
+            Add Resident</button>
+      </div>
     );
-  }
 }
+
+export default AddNewResident;

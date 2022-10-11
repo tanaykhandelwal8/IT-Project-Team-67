452b69ec302b89efebfdf38f9c6d6b0a3edb1c53
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-11 12:32:26 +1100

    added routes for preference pages

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,14 @@ app.use(express.static(__dirname))
 
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose')
-const Residents = require("./models/resident")
+
 var residentRouter = require("./routes/residentRouter")
+
+const Residents = require("./models/resident")
+const Musics = require("./models/music")
+const Foods = require("./models/food")
+const Animals = require("./models/animal")
+const Movies = require("./models/movie")
 
 var fs = require('fs');
 var path = require('path');
@@ -22,6 +28,7 @@ app.use(bodyParser.json())
 app.set("view engine", "ejs");
 
 var multer = require('multer');
+const Music = require('./models/music')
   
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -48,6 +55,46 @@ app.get('/view-all-residents', async (req, res) => {
         res.send(residents)
     }   else {
         res.send( {result: "No Residents Found"})
+    }
+
+})
+
+app.get('/music', async (req, res) => {
+    const musics = await Musics.find()
+    if (musics.length > 0){
+        res.send(musics)
+    }   else {
+        res.send( {result: "No music Found"})
+    }
+
+})
+
+app.get('/foods', async (req, res) => {
+    const foods = await Foods.find()
+    if (foods.length > 0){
+        res.send(foods)
+    }   else {
+        res.send( {result: "No foods Found"})
+    }
+
+})
+
+app.get('/animals', async (req, res) => {
+    const animals = await Animals.find()
+    if (animals.length > 0){
+        res.send(animals)
+    }   else {
+        res.send( {result: "No animals Found"})
+    }
+
+})
+
+app.get('/movies', async (req, res) => {
+    const movies = await Movies.find()
+    if (movies.length > 0){
+        res.send(movies)
+    }   else {
+        res.send( {result: "No movies Found"})
     }
 
 })

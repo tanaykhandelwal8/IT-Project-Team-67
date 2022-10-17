b51242051ec70ed606ebbe4f8e49a2f4162471b8
jettm100 <80664331+jettm100@users.noreply.github.com>
2022-10-17 18:26:48 +1100

    footer displays logged in resident

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import {useParams} from "react-router-dom";
+import axios from 'axios';
 
 function Footer() {
+
+  const [staffData, setStaffData] = useState([{}])
+  const getStaffData = () => {
+    axios.get("http://localhost:3001/get-staff-data")
+    .then((res) => {setStaffData(res.data)})
+  }
+  getStaffData()
+
+  const [residentData, setResidentData] = useState([{}])
+  const getResidentData = () => {
+    axios.get("http://localhost:3001/get-resident-data")
+    .then((res) => {setResidentData(res.data)})
+  }
+  getResidentData()
+
+  const params = useParams();
+  const userID = params.id;
+
   return (
     <nav>
         <footer style={{zIndex:"109", position:"fixed"}}>
             <p style={{"float": "left", "margin-left": "5vw"}}><i>Residencely</i></p>
-            <p style={{"float":"right", "margin-right": "5vw"}}>Signed in as Xxxx Xxxxxx</p>
+            {residentData.map((user, key) => (
+              (user._id == userID) ? <p style={{"float":"right", "margin-right": "5vw"}}>Signed in as {user.firstName} {user.lastName}</p> : <div></div>
+            ))}
         </footer>
     </nav>
   );

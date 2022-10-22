079ac1fa4dfeefe09afbf1ab808c82b33df862b4
Tanay Khandelwal <khandelwalt@student.unimelb.edu.au>
2022-10-22 21:05:59 +1100

    heroku build

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const Movies = require("./models/movie")
 
 
 var fs = require('fs');
-var path = require('path');
+const path = require('path');
 require('dotenv/config');
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -327,3 +327,10 @@ app.listen(process.env.PORT, () => {
     console.log('Listening on Port ' + process.env.PORT);
 })
 
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})

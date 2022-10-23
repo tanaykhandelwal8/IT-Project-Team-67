4c004c2f57014c867748ad0d7ac5837e2db57d33
Tanay Khandelwal <khandelwalt@student.unimelb.edu.au>
2022-10-23 16:16:44 +1100

    heroku

--- index.js
+++ index.js
@@ -323,10 +323,6 @@ app.listen(process.env.PORT, () => {
     console.log('Listening on Port ' + process.env.PORT);
 })
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+}

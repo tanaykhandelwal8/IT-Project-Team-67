386982d1d14a34be1c05021952fcfe53a1b24c5a
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-30 17:43:30 +1100

    reordering index

--- backend/index.js
+++ backend/index.js
@@ -109,6 +109,18 @@ app.use("/staff", staffRouter);
 const authRouter = require('./routes/auth')
 app.use(authRouter.router)
 
+app.listen(process.env.PORT | PORT, () => {
+    console.log('Listening on Port ' + process.env.PORT);
+})
+
+// Serve static files from the React frontend app
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+})
+
 app.post('/view-other-resident', (req, res) => {
     console.log("HERE");
 })
@@ -467,14 +479,4 @@ app.post('/delete-event', (req, res) =>{
         });
 })
 
-app.listen(process.env.PORT | PORT, () => {
-    console.log('Listening on Port ' + process.env.PORT);
-})
 
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-})

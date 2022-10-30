40eb38b7dde9fae7bea7215aca2954da57b20f6b
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-31 09:56:10 +1100

    add production clause

--- index.js
+++ index.js
@@ -12,13 +12,31 @@ app.listen(process.env.PORT | PORT, () => {
     console.log('Listening on Port ' + process.env.PORT);
 })
 
+/*
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+//app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static('client/build'))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+    //res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) // relative path
 })
+*/
+//const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static("client/build"));
+
+    app.get("*", (req, res) => {
+
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+
+   });
+
+}
+
 
 require('./models/index')
 app.use(express.static(__dirname))

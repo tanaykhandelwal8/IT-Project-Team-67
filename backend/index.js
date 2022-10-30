b8404761cc5edebdd0bc35fb397eaa2b6039e133
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-30 17:55:15 +1100

    edit

--- backend/index.js
+++ backend/index.js
@@ -118,7 +118,7 @@ app.use(express.static(path.join(__dirname, '../frontend/build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+    res.sendFile(path.join(__dirname + '/./frontend/build/index.html'))
 })
 
 app.post('/view-other-resident', (req, res) => {

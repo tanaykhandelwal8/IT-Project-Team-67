f0baeb4c030400f52fe29e0a3f4b51bd68d287b4
Panos Menidis <panosmenidis@panoss-mbp.lan>
2022-10-31 12:23:17 +1100

    client to frontend

--- index.js
+++ index.js
@@ -25,16 +25,11 @@ app.get('*', (req, res) => {
 */
 //const path = require("path");
 
-if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static("client/build"));
-
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static("frontend/build"))
     app.get("*", (req, res) => {
-
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-
-   });
-
+        res.snedFile(path.resolve(__dirname,"frontend","build","index.html"))
+    })
 }
 
 

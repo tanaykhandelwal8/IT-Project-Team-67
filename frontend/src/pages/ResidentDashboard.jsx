92c9422d586567954dbd17649c23137c34583a48
jettm100 <80664331+jettm100@users.noreply.github.com>
2022-11-07 11:44:32 +1100

    heroku .map fix test

--- frontend/src/pages/ResidentDashboard.jsx
+++ frontend/src/pages/ResidentDashboard.jsx
@@ -80,7 +80,7 @@ function ResidentDashboard(props) {
         <div className='Font'>
         <div>
             <div className="dashboard-title">
-                {residentData.map((user, key) => (
+                {[residentData][0].map((user, key) => (
                     <div>
                     {(user._id === userID) ? <h1 style={{display: "inline"}}>Welcome {user.firstName} {user.lastName}</h1> : <div></div>}
                     {(user._id === userID) ? <Link to="../edit-resident-dashboard" style={{float: "right"}}>Edit Profile</Link> : <div></div>}
@@ -88,6 +88,7 @@ function ResidentDashboard(props) {
                 ))}
             </div>
         </div>
+        {/*
         <div className="row">
             <div className="left-column">
                 <div className="gallery-card">
@@ -338,6 +339,7 @@ function ResidentDashboard(props) {
                 </div>
             </div>
         </div>
+        */}
     </div>
     );
 }

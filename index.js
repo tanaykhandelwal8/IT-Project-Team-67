8c762b3613584677e1a43ce2f0dd56d221bbe802
pieceoftrash8903 <56954478+pieceoftrash8903@users.noreply.github.com>
2022-11-08 12:42:41 +1100

    moved get * to bottom

--- index.js
+++ index.js
@@ -25,14 +25,6 @@ app.get('*', (req, res) => {
 */
 //const path = require("path");
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("frontend/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
-    })
-}
-
-
 require('./models/index')
 app.use(express.static(__dirname))
 
@@ -493,4 +485,9 @@ app.post('/delete-event', (req, res) =>{
         });
 })
 
-
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static("frontend/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+    })
+}

566afc12d1855b6aa67948ac7b0123f88f2ca247
Tanay Khandelwal <khandelwalt@student.unimelb.edu.au>
2022-11-10 15:53:16 +1100

    refactored and commented code

--- index.js
+++ index.js
@@ -3,44 +3,16 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
-
+/************************* IMPORTS ******************************/
 const app = express()
 const path = require('path');
 const PORT = process.env.PORT || 3001
-
-app.listen(process.env.PORT | PORT, () => {
-    console.log('Listening on Port ' + process.env.PORT);
-})
-
-/*
-// Serve static files from the React frontend app
-//app.use(express.static(path.join(__dirname, '../frontend/build')))
-app.use(express.static('client/build'))
-
-// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
-app.get('*', (req, res) => {
-    //res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')) // relative path
-})
-*/
-//const path = require("path");
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("frontend/build"))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
-    })
-}
-
-
-require('./models/index')
-app.use(express.static(__dirname))
-
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
-
-//var residentRouter = require("./routes/residentRouter")
-
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcryptjs')
+const session = require('express-session')
+const flash = require('express-flash')
+const passportlocal = require('passport-local').Strategy
 const Residents = require("./models/resident")
 const Musics = require("./models/music")
 const Foods = require("./models/food")
@@ -50,19 +22,80 @@ const Hobby = require("./models/hobby")
 const Languages = require("./models/language")
 const Musicians = require("./models/musician")
 const Interests = require("./models/interest")
-
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
 var fs = require('fs');
 require('dotenv/config');
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-
-// Set EJS as templating engine
-app.set("view engine", "ejs");
-
 const multer = require('multer');
 const ImageModel = require("./models/image")
+const residentRouter = require("./routes/residentRouter");
+const staffRouter = require("./routes/staffRouter");
+const passport = require('./config/passport')
+require('./models/index')
+const authRouter = require('./routes/auth')
+require('./models')
+const resident = require('./models/resident')
+const staff = require('./models/staff')
+const calendar = require('./models/calendar')
+const music = require('./models/music')
+const interest = require('./models/interest')
+const food = require('./models/food')
+const hobby = require('./models/hobby')
+const musician = require('./models/musician')
+const animals = require('./models/animal')
+const movies = require('./models/movie')
+const language = require('./models/language')
 
+
+
+// if on heroku, use the build folder in the frontend to deploy
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static("frontend/build"))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname,"frontend","build","index.html"))
+    })
+}
+
+/************************* MIDDLEWARE ******************************/
+app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors({
+    origin: "https://it-project-team-67.herokuapp.com",
+    credentials: true
+}))
+app.use(session({
+    secret: "secretcode",
+    resave: true,
+    saveUninitialized:true
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(cookieParser("secretcode"))
+
+app.use(flash())
+
+app.use("/resident", residentRouter);
+app.use("/staff", staffRouter);
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+app.use(authRouter.router)
+
+
+
+
+/********************************** IMAGES FUNCTIONALITY ******************************/
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, 'uploads')
@@ -91,90 +124,12 @@ app.post('/post', (req, res) => {
         }
     })
 })
-// Imports
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
-const bcrypt = require('bcryptjs')
-const session = require('express-session')
-const flash = require('express-flash')
-const passportlocal = require('passport-local').Strategy
-
-// middleware
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cors({
-    origin: "https://it-project-team-67.herokuapp.com",
-    credentials: true
-}))
-app.use(session({
-    secret: "secretcode",
-    resave: true,
-    saveUninitialized:true
-}))
-const passport = require('./config/passport')
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use(cookieParser("secretcode"))
-
-app.use(flash())
-
-const residentRouter = require("./routes/residentRouter");
-const staffRouter = require("./routes/staffRouter");
-app.use("/resident", residentRouter);
-app.use("/staff", staffRouter);
-
-// Add Access Control Allow Origin headers
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-
-/*app.post('/login', (req,res) => {
-    console.log("HELLO");
-    console.log(req.body);
-
-})
-*/
-
-const authRouter = require('./routes/auth')
-app.use(authRouter.router)
+/********************************** IMAGES FUNCTIONALITY******************************/
 
 
+/********************************* FETCHING DATA FROM THE BACKEND ***************************************/
 app.post('/view-other-resident', (req, res) => {
     console.log("HERE");
-})
-
-require('./models')
-const resident = require('./models/resident')
-const staff = require('./models/staff')
-const calendar = require('./models/calendar')
-const music = require('./models/music')
-const interest = require('./models/interest')
-const food = require('./models/food')
-const hobby = require('./models/hobby')
-const musician = require('./models/musician')
-const animals = require('./models/animal')
-const movies = require('./models/movie')
-const language = require('./models/language')
-
-app.get('/get-staff-data', (req, res) => {
-    staff.find().then((result) => {
-        res.json(result)
-    }).catch((err) => {
-        console.error(err)
-    })
-})
-app.get('/get-events-data', (req, res) => {
-    calendar.find().then((result) => {
-        res.json(result)
-    }).catch((err) => {
-        console.error(err)
-    })
 })
 
 app.get('/get-music-data', (req, res) => {
@@ -241,35 +196,91 @@ app.get('/get-animal-data', (req, res) => {
     })
 })
 
-app.get('/user', (req, res) => {
+app.get('/view-all-residents', async (req, res) => {
+    const residents = await Residents.find()
+    if (residents.length > 0){
+        res.send(residents)
+    }   else {
+        res.send( {result: "No Residents Found"})
+    }
 
 })
 
-app.get('/success', (req, res) => {
-    res.send('success')
+app.get('/music', async (req, res) => {
+    const musics = await Musics.find()
+    if (musics.length > 0){
+        res.send(musics)
+    }   else {
+        res.send( {result: "No music Found"})
+    }
 
 })
 
-app.get('/failure', (req, res) => {
-    res.send('failure')
+app.get('/hobby', async (req, res) => {
+    const hobby = await Hobby.find()
+    if (hobby.length > 0){
+        res.send(hobby)
+    }   else {
+        res.send( {result: "No hobbies Found"})
+    }
 
 })
+
+app.get('/foods', async (req, res) => {
+    const foods = await Foods.find()
+    if (foods.length > 0){
+        res.send(foods)
+    }   else {
+        res.send( {result: "No foods Found"})
+    }
+})
+
+app.get('/animal', async (req, res) => {
+    const animals = await Animals.find()
+    if (animals.length > 0){
+        res.send(animals)
+    }   else {
+        res.send( {result: "No animals Found"})
+    }
+
+})
+
+app.get('/movies', async (req, res) => {
+    const movies = await Movies.find()
+    if (movies.length > 0){
+        res.send(movies)
+    }   else {
+        res.send( {result: "No movies Found"})
+    }
+
+})
+
+
+app.get('/get-staff-data', (req, res) => {
+    staff.find().then((result) => {
+        res.json(result)
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+app.get('/get-events-data', (req, res) => {
+    calendar.find().then((result) => {
+        res.json(result)
+    }).catch((err) => {
+        console.error(err)
+    })
+})
+
+
 
 // 404 for everything else
 app.get('*', function(req, res){
     res.status(404).send('404');
 });
 
+/********************************* FETCHING DATA FROM THE BACKEND ***************************************/
 
-/*
-app.post('/login', (req, res, next) => {
-    passport.authenticate('resident-local', {
-        successRedirect:'success',
-        failureRedirect:'failure',
-        failureFlash: true
-    })
-})
-*/
+/************************* POST ROUTES TO SEND DATA TO THE BACKEND ******************************/
 app.post('/register-resident', (req, res) => {
     resident.findOne({email:req.body.email}, async (err, doc) => {
         if(err) throw err
@@ -403,70 +414,8 @@ app.post('/update-language-preferences', async (req, res) => {
 
 })
 
-//app.use('/resident', residentRouter);
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
-})
 
-app.get('/view-all-residents', async (req, res) => {
-    const residents = await Residents.find()
-    if (residents.length > 0){
-        res.send(residents)
-    }   else {
-        res.send( {result: "No Residents Found"})
-    }
-
-})
-
-app.get('/music', async (req, res) => {
-    const musics = await Musics.find()
-    if (musics.length > 0){
-        res.send(musics)
-    }   else {
-        res.send( {result: "No music Found"})
-    }
-
-})
-
-app.get('/hobby', async (req, res) => {
-    const hobby = await Hobby.find()
-    if (hobby.length > 0){
-        res.send(hobby)
-    }   else {
-        res.send( {result: "No hobbies Found"})
-    }
-
-})
-
-app.get('/foods', async (req, res) => {
-    const foods = await Foods.find()
-    if (foods.length > 0){
-        res.send(foods)
-    }   else {
-        res.send( {result: "No foods Found"})
-    }
-})
-
-app.get('/animal', async (req, res) => {
-    const animals = await Animals.find()
-    if (animals.length > 0){
-        res.send(animals)
-    }   else {
-        res.send( {result: "No animals Found"})
-    }
-
-})
-
-app.get('/movies', async (req, res) => {
-    const movies = await Movies.find()
-    if (movies.length > 0){
-        res.send(movies)
-    }   else {
-        res.send( {result: "No movies Found"})
-    }
-
-})
 
 /* sending a new event to mongo */
 app.post('/add-event', (req, res) => {
@@ -503,4 +452,8 @@ app.post('/delete-event', (req, res) =>{
         });
 })
 
+/************************* POST ROUTES TO SEND DATA TO THE BACKEND ******************************/
 
+app.listen(process.env.PORT | PORT, () => {
+    console.log('Listening on Port ' + process.env.PORT);
+})
